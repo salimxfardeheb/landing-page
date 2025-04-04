3b43ac4eb68594d11f06ec6eea90e138e8b70b62
salimxfardeheb <salimfardeheb442@gmail.com>
2025-04-05 00:01:24 +0100

    initialize project

---
+++ landing-page-template/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+      <div className='w-full h-screen bg-amber-400 flex justify-center items-center text-9xl font-bold'>Hello</div>
+    </>
+  )
+}
+
+export default App
